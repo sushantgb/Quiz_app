9571602dfcdf00f4e461f41e57fb191dc1c96044
Sushant Ganeshan <sgbharat.joint@gmail.com>
2022-06-12 13:28:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ var storedName = localStorage.getItem("storedName");
 function loadName(){
     var userName = document.getElementById('username').value;
     localStorage.setItem("storedName", userName); //storing username in 'storedName'
-    if(localStorage.length == ""){
-        //showing error if name is not entered
-        alert("Please enter your name");
-        console.log("error: name is not entered"); 
-    }
 }
 
 //keeping buttons disabled till the name is entered
 if (localStorage.length == ""){
+    //showing error if name is not entered
+    alert("Please enter your name");
+    console.log("error: name is not entered");
     btn1.disabled = true;
     btn2.disabled = true;
     btn3.disabled = true;
