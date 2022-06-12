5a5cd3d92bcd599cbb7beec9bb8d1db115a60629
Sushant Ganeshan <sgbharat.joint@gmail.com>
2022-06-12 13:18:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 //related to index page
 
+var btn1 = document.getElementById('his');
+var btn2 = document.getElementById('sci');
+var btn3 = document.getElementById('pol');
+var btn4 = document.getElementById('geo');
+var user = document.getElementById("username");
+
 //storing the value loacally
-var storedName = localStorage.getItem("storedName");
+var storedName = localStorage.getItem("storedName"); 
+
 function loadName(){
     var userName = document.getElementById('username').value;
     localStorage.setItem("storedName", userName); //storing username in 'storedName'
+    if(userName == ""){
+        //showing error if name is not entered
+        alert("Please enter your name");
+        console.log("error: name is not entered"); 
+    }
 }
-console.log(storedName); //for reference and debugging
+
+//keeping buttons disabled till the name is entered
+if (localStorage.length == ""){
+    btn1.disabled = true;
+    btn2.disabled = true;
+    btn3.disabled = true;
+    btn4.disabled = true;
+}
+
+//for debugging purpose
+console.log(localStorage.getItem("storedName"))
+console.log("Length : " + localStorage.length);
